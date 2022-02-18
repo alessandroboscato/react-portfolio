9c01a02de2e778fa7dbea7096370bd5194a63011
alebsct <alessandro.boscato@gmail.com>
2022-02-18 11:48:48 +1100

    Start project

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+      title: "Boolflix",
+      subtitle: "HTML/CSS/jQuery/AJAX",
+      description:
+        "In this work I used AJAX calls via jQuery to recreate the well-known Netflix streaming service. For the graphic part I used HTML5, CSS and JavaScript.",
+      image: "./boolflix.jpg",
+    },
+    {
+        title: "Hubspot",
+        subtitle: "HTML/CSS",
+        description:
+          "In the present work I have recreated the Hubspot home page layout using HTML5 and CSS3.",
+        image: "./hubspot.jpg",
+      },
+      {
+        title: "Digital Ocean",
+        subtitle: "HTML/CSS/JS responsive",
+        description:
+          "In this work I have recreated the Digital Ocean home page layout using HTML5 and CSS3. The site is responsive.",
+        image: "./digitalocean.jpg",
+      },
+      {
+        title: "Spotify web - Single Page Application",
+        subtitle: "HTML/CSS/JS responsive",
+        description:
+          "In this work I have recreated the Sporify Web application using HTML5 and CSS3, all in responsive mode.",
+        image: "./spotify.jpg",
+      },
+      {
+        title: "Whatsapp web",
+        subtitle: "HTML/CSS/JS",
+        description:
+          "In this work the well-known Whatsapp web application has been reproduced using HTML5, CSS3 and JavaScript.",
+        image: "./whatsapp.jpg",
+      }
+  ];
---
+++ src/data2.js
@@ -0,0 +1,27 @@
+export const skills = [
+    {
+      skill: "HTML",
+    },
+    {
+      skill: "CSS/SASS/Tailwind",
+    },
+    {
+      skill: "JS/jQuery/React",
+    },
+    {
+      skill: "PHP/Laravel/CodeIgniter",
+    },
+    {
+      skill: "GIT/GitHub",
+    },
+    {
+      skill: "MYSQL",
+    },
+    {
+      skill: "Docker",
+    },
+    {
+      skill: "TDD/PHP Unit",
+    }
+
+  ];
