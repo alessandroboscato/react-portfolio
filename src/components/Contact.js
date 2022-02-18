c6ef69646d150e1a090c80a0af5523c7a7615b51
alebsct <alessandro.boscato@gmail.com>
2022-02-18 17:12:21 +1100

    Deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,9 +53,9 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <p className="text-indigo-400 leading-relaxed">
                 alessandro.boscato@gmail.com
-              </a>
+              </p>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
