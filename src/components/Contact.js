7647fd03d6c17941044deb0dc8b45b8338da98fe
alebsct <alessandro.boscato@gmail.com>
2022-05-17 08:51:52 +0200

    change address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,7 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=hastings,+victoria&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed/v1/place?q=via+udine+36+creazzo&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
@@ -45,8 +45,8 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                3 Lefroy Lane <br />
-                Hastings, Victoria, 3915
+                Via Udine, 36 <br />
+                Creazzo, Vicenza, Italy
               </p>
             </div>
             <div className="lg:w-1/2 pl-6 pr-10 mt-4 lg:mt-0">
@@ -59,7 +59,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">0455457557</p>
+              <p className="leading-relaxed">+393889813853</p>
             </div>
           </div>
         </div>
